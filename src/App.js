75e2331cf9ba90847af78ccd6fb2551f13c47977
todaykim <pztdrd@naver.com>
2021-07-29 14:33:02 +0900

    show video list with title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './app.css';
+import VideoList from './component/video_list/videoList';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow",
+    };
+
+    fetch(
+        "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCJZMxB6LjVN2-ndvDzpsToffcCjHiFD8g",
+        requestOptions
+    )
+        .then((response) => response.json())
+        .then((result) => setVideos(result.items))
+        .catch((error) => console.log("error", error));
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VideoList videos={videos} />
   );
 }
 
